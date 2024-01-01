119eccf007d2db88c01f465695528f213cda0ed9
yusuf umar faruq <luwaservices@gmail.com>
2024-01-01 11:24:06 -0700

    refactored createPineconeIndex.js

--- createPineconeIndex.js
+++ createPineconeIndex.js
@@ -2,13 +2,9 @@ const createPineconeIndex = async (client, indexName, vectorDimension) => {
     const existingIndexes = await client.listIndexes();
     const checkIndex = existingIndexes.find(index => index.name === indexName);
 
-
-    // //check if new index is available
     if (checkIndex) {
-        console.log(`index name ${indexName} already exists.`)
         return;
     } else {
-        //create pinecone index
         await client.createIndex({
             name: indexName,
             dimension: vectorDimension,
